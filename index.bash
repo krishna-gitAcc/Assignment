2d598cfd17c7268ddb1d5c5a2480f1b00f38be60
Kapil <krishna0512.kr@gmail.com>
2024-10-18 13:55:34 +0530

    bash command to create folder and each folder contain a json file.

---
+++ index.bash
@@ -0,0 +1,6 @@
+# command to make multiple file like project1 to project 10
+mkdir project_{1..10}_{backend,frontend}
+
+# command to create a package.json file in each project file.
+
+touch project_{1..10}_{backend,fronend}/package.json
