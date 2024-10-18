9b5737d9bdf982e852cc99db09a8797e92423335
Kapil <krishna0512.kr@gmail.com>
2024-10-18 10:31:40 +0530

    initial commit.

---
+++ dsa.py
@@ -0,0 +1,30 @@
+
+from typing import List
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=[]):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+
+    def createTree(input: List):
+        pass
+
+    def postorder(self, root: Node) -> List[int]:
+        pass
+
+
+"""
+Input: root = [1,null,3,2,4,null,5,6]
+"""
+
+"""
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+"""
+
+# create a file which file
+
